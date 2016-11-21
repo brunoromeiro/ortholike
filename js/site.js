bfa708a1eb2576b155dfc08f3cae046c7773a4b3
brunoromeiro <bruno.romeiro@gmail.com>
2016-11-21 08:08:04 -0200

    Atualizaçao
    Atualizando Pagina

---
+++ js/site.js
@@ -0,0 +1,8 @@
+$('.topo').on('click', myFunction);
+
+/*function MeuTopo(){
+	$('html,body').animate({scrollTop: 0},'fast');
+}*/
+ $('#somewhere').click(function(){
+   $('html, body').animate({scrollTop:0}, 'slow');
+ });
